17707a524d0f4a02dde0c46f14c16d4d2a1e3623
zew644270-afk <zew644270-afk@users.noreply.github.com>
2026-09-05 19:03:27 +0800

    deploy v0.4: Case Coach 可发现性优化（示例问题/欢迎语/悬浮入口）

---
+++ assets/swimlanesDiagram-VR7AAH4N-Bm9J1i3G.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW-BB8OVFl0.js";import{_ as a}from"./MermaidFlow-CKCgUxDQ.js";import"./chunk-5VM5RSS4-BVY7PCkv.js";import"./chunk-XXDRQBXY-BG2FUHvW.js";import"./chunk-POPQ4Y6H-DyhcExJD.js";import"./chunk-F27PBJKO-B0QD3SAQ.js";import"./channel-CuvuQ_Mw.js";import"./index-ojpIYx7W.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
